2dd69c6720b979588b268998309186ca77bde68b
Danila <syvs0990@mail.ru>
2022-04-06 19:08:34 +0700

    Added node-sass and created user list

---
+++ src/components/UserList/ListItem.vue
@@ -0,0 +1,43 @@
+<template>
+    <div className="user-list__item">
+        <div className="user-list__data">
+            <div className="user-list__info">
+                <div className="user-list__item-title">Name:</div>
+                <div className="user-list__text">{{item.name}}</div>
+            </div>
+            <div className="user-list__info">
+                <div className="user-list__item-title">City:</div>
+                <div className="user-list__text">{{item.address.city}}</div>
+            </div>
+            <div className="user-list__info">
+                <div className="user-list__item-title">Company:</div>
+                <div className="user-list__text">{{item.company.name}}</div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'ListItem',
+        props: ['item']
+    }
+</script>
+
+<style>
+    .user-list__data {
+        display: flex;
+        flex-direction: column;
+        row-gap: 3px;
+    }
+
+    .user-list__info {
+        display: flex;
+    }
+
+    .user-list__item-title {
+        box-sizing: border-box;
+        width: 90px;
+        font-weight: 500;
+    }
+</style>
---
+++ src/components/UserList/UserList.vue
@@ -0,0 +1,45 @@
+<template>
+    <div className="user-list block">
+        <div className="container">
+            <div className="user-list__content content">
+                <div className="user-list__title title" v-on:click="seen = !seen" >User list</div>
+                <div v-if="seen" className="user-list__list">
+                   <ListItem v-for="el in arr" v-bind:key="el" v-bind:item="el"/>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import ListItem from './ListItem.vue'
+    export default {
+        name: 'App',
+        data () {
+            return {
+                arr: [],
+                seen: false
+            }
+        },
+        mounted () {
+            fetch('https://jsonplaceholder.typicode.com/users')
+            .then(response => response.json())
+            .then(data => { this.arr = data })
+        },
+        components: {
+            ListItem
+        }
+    }
+</script>
+
+<style>
+    .user-list__list {
+        display: flex;
+        flex-direction: column;
+        row-gap: 20px;
+        border: 1px solid #dddddd;
+        background-color: #f7f7f7;
+        padding: 10px 20px;
+        border-radius: 5px;
+    }
+</style>
