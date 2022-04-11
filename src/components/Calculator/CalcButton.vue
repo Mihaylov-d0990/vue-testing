40d1ac33113d6a1ca5bc3095362dec2aa6263f55
Danila <syvs0990@mail.ru>
2022-04-11 21:46:32 +0700

    Disabled ESlint and created calc button component

---
+++ src/components/Calculator/CalcButton.vue
@@ -0,0 +1,21 @@
+<template>
+    <button class="calculator__button">
+        <slot></slot>
+    </button>
+</template>
+
+<script>
+export default {
+    name: 'calc-button'
+}
+</script>
+
+<style scoped>
+    .calculator__button {
+        border: 1px solid #dddddd;
+        background-color: #f7f7f7;
+        border-radius: 5px;
+        cursor: pointer;
+        transition: 0.3s;
+    }
+</style>
--- src/components/Calculator/Calculator.vue
+++ src/components/Calculator/Calculator.vue
@@ -3,22 +3,46 @@
         <div class="container">
             <div class="calculator__content content">
                 <div class="calculator__title title" @click="seen = !seen">Calculator</div>
+                <div class="calculator__form" v-if="seen">
+                    <input type="text" class="calculator__input" />
+                    <div class="calculator__buttons">
+                        <calc-button v-for="value in buttons" >{{value}}</calc-button>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
 </template>
 
 <script>
+import CalcButton from "./CalcButton"
 export default {
     name: 'calculator-component',
     data () {
         return {
-            seen: false
+            seen: true,
+            buttons: ["1", "2", "3", "+", 
+                "4", "5", "6", "-", 
+                "7", "8", "9", "*",
+                "←", "0", "=", "/"]
         }
+    },
+    components: {
+        CalcButton
     }
 }
 </script>
 
 <style>
-
+    .calculator__form {
+        border: 1px solid #dddddd;
+        background-color: #f7f7f7;
+        padding: 5px;
+        border-radius: 5px;
+        display: flex;
+        flex-direction: column;
+        align-items: flex-start;
+        align-self: flex-start;
+        row-gap: 3px;
+    }
 </style>
