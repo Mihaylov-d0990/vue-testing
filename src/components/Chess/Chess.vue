7479741ae379e5f2e4e2c130cb4147f3d838cc25
Danila <syvs0990@mail.ru>
2022-04-15 17:40:35 +0700

    Created chess figures and initialized them

--- src/components/Chess/Chess.vue
+++ src/components/Chess/Chess.vue
@@ -5,11 +5,13 @@
                 <div class="chess__title title" @click="seen = !seen">Chess</div>
                 <div class="chess__fields" v-if="seen">
                     <div
-                        :class="['chess__field']"
+                        :class="[field.figure ? 'chess__figure' : '', 'chess__field']"
                         v-for="field in fields"
                         :key="field.id"
                         @click="displayFigure(field.figure)"
-                    >{{field.id}}</div>
+                    >
+                        <img v-if="field.figure" :src="field.figure.image" alt="">
+                    </div>
                 </div>
             </div>
         </div>
@@ -17,23 +19,17 @@
 </template>
 
 <script>
-
+import { createChessBoard, initializeFigures } from "./initialization"
 export default {
     name: 'chess-component',
     data () {
         return {
             seen: true,
-            fields: (() => {
-                let arr = []
-                for (let i = 0; i < 64; i++) {
-                    arr.push({
-                        id: i,
-                        figure: null
-                    })
-                }
-                return arr
-            })()
+            fields: createChessBoard()
         }
+    },
+    mounted () {
+        this.fields = initializeFigures(this.fields)
     },
     methods: {
         displayFigure(figure) {
@@ -52,32 +48,44 @@ export default {
     }
 
     .chess__field {
-        border: 1px solid black;
         background-color: #ffffff;
+        display: flex;
+        justify-content: center;
     }
 
     .chess__field:nth-child(n + 1):nth-child(-n + 8):nth-child(even) {
-      background-color: #000000; }
+      background-color: #727272; }
 
     .chess__field:nth-child(n + 8):nth-child(-n + 16):nth-child(odd) {
-      background-color: #000000; }
+      background-color: #727272; }
 
     .chess__field:nth-child(n + 17):nth-child(-n + 24):nth-child(even) {
-      background-color: #000000; }
+      background-color: #727272; }
 
     .chess__field:nth-child(n + 24):nth-child(-n + 32):nth-child(odd) {
-      background-color: #000000; }
+      background-color: #727272; }
 
     .chess__field:nth-child(n + 33):nth-child(-n + 40):nth-child(even) {
-      background-color: #000000; }
+      background-color: #727272; }
 
     .chess__field:nth-child(n + 40):nth-child(-n + 48):nth-child(odd) {
-      background-color: #000000; }
+      background-color: #727272; }
 
     .chess__field:nth-child(n + 49):nth-child(-n + 56):nth-child(even) {
-      background-color: #000000; }
+      background-color: #727272; }
 
     .chess__field:nth-child(n + 56):nth-child(-n + 64):nth-child(odd) {
-      background-color: #000000; }
+      background-color: #727272; }
+
+    .chess__field img {
+        padding: 3px;
+        object-fit: cover;
+        object-position: 50% 50%;
+        height: 100%;
+    }
+
+    .chess__figure {
+        cursor: pointer;
+    }
 
 </style>
---
+++ src/components/Chess/initialization.js
@@ -0,0 +1,84 @@
+const createChessBoard = () => {
+    let arr = []
+    for (let i = 0; i < 64; i++) {
+        arr.push({
+            id: i,
+            figure: null
+        })
+    }
+    return arr
+}
+
+const initializeFigures = (chessBoard) => {
+    const pawFill = (first, last, color) => {
+        for (let i = first; i < last; i++) {
+            chessBoard[i].figure = {
+                priority: 1,
+                image: require(`../../assets/chesses/${color}-1.png`)
+            }
+        }
+    }
+
+    pawFill(48, 56, 'w')
+
+    chessBoard[57].figure = chessBoard[62].figure = {
+        priority: 2,
+        image: require('../../assets/chesses/w-2.png')
+    }
+
+    chessBoard[58].figure = chessBoard[61].figure = {
+        priority: 3,
+        image: require('../../assets/chesses/w-3.png')
+    }
+
+    chessBoard[56].figure = chessBoard[63].figure = {
+        priority: 4,
+        image: require('../../assets/chesses/w-4.png')
+    }
+
+    chessBoard[59].figure = {
+        priority: 5,
+        image: require('../../assets/chesses/w-5.png')
+    }
+
+    chessBoard[59].figure = {
+        priority: 5,
+        image: require('../../assets/chesses/w-5.png')
+    }
+
+    chessBoard[60].figure = {
+        priority: 6,
+        image: require('../../assets/chesses/w-6.png')
+    }
+
+    pawFill(8, 16, 'b')
+
+    chessBoard[1].figure = chessBoard[6].figure = {
+        priority: 2,
+        image: require('../../assets/chesses/b-4.png')
+    }
+
+    chessBoard[2].figure = chessBoard[5].figure = {
+        priority: 3,
+        image: require('../../assets/chesses/b-3.png')
+    }
+
+    chessBoard[0].figure = chessBoard[7].figure = {
+        priority: 4,
+        image: require('../../assets/chesses/b-2.png')
+    }
+
+    chessBoard[3].figure = {
+        priority: 5,
+        image: require('../../assets/chesses/b-5.png')
+    }
+
+    chessBoard[4].figure = {
+        priority: 6,
+        image: require('../../assets/chesses/b-6.png')
+    }
+
+    return chessBoard
+}
+
+export { createChessBoard, initializeFigures }
